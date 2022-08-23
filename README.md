257dd50e77cc0a8befd5f0128e7344ed0e1bbe62
Eva-Li8E <evaliboden@gmail.com>
2022-08-23 20:30:27 +0200

    update readme filen

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 Min tanke var att gå rakt på VG delen och det gjorde jag. Jag startade upp ett project genom git hub och bydggde och bröt ut projectet. Men tyvär så tog jag mig nog vatten över huvudet, jag lyckades bara få till get all och get id med json filen. Så jag tog beslutet att starta ett nytt project och satsa på G delen i första hand..och om tid finns försöka mig på vg delen igen. Så detta är mitt andra project!
 
 Jag valde att projectet skulle handla om böcker.
+
+1. Projektet innehåller minst 4 st. endpoints (GET, POST, PUT & DELETE för en resurs) svar = Ja samtliga finns plus en get get id
+2. Samtliga endpoints skall kunna nås via en REST Client fil (.rest|.http) Svar = Ja Endpoins nås via
+   book.rest filen
+3. Datan som API:et hanterar sparas lokalt i serverfilen svar = ja
+4. APIét ska svara med 404 om datan saknas. svar = ja
+5. Git & GitHub har använts Svar = ja
+6. Projektmappen innehåller en README.md fil - (läs ovan för mer info) svar = ja det är den vi befinner oss i nu
+7. Uppgiften lämnas in i tid! svar = ja den lämnades in datum:
+
+Detta projectet började jag med att lägga allt i en fil och sen bröt jag ut det till flera filer.
+Min önskan var som sagt att satsa på vg delen. Men då jag började i fel ende så räckte inte tiden till tillslut.
+
+server filen = Det var i denna fil som jag la allt först
+bookController = Men flyttade sedan arrayen med datan till bookcontrollen och skapade de olika metoderna
+bookrouter = tar jag in metoderna i router mappen för att ha alla endpoints i en och samma fil
+book.rest = i denna fil testar man de olika endpointsen
+
+Av Eva-i Nätynki Bodén
--- src/server.ts
+++ src/server.ts
@@ -1,77 +1,9 @@
 import express from "express";
-import internal from "stream";
 import bookRouter from "./bookresourses/bookrouter";
 
 const app = express();
-
 app.use(express.json());
-
-// interface Book {
-//   id: number;
-//   title: string;
-//   author: string;
-//   isbn: number;
-//   pages: number;
-//   published: number;
-// }
-
 app.use("/api/books", bookRouter);
-
-// app.get("/", (req, res) => {
-//   res.status(200).send(books);
-// });
-
-// app.get("/api/books/:id", (req, res) => {
-//   const book = books.find(
-//     (b: { id: number }) => b.id === parseInt(req.params.id)
-//   );
-//   if (!book) res.status(404).send("BookId not found!");
-//   res.send(book);
-// });
-
-// app.post("/api/books", (req, res) => {
-//   if (!req.body.title || req.body.title.length < 3) {
-//     res.status(404).send("Must contain at least 3 letters..");
-//     return;
-//   }
-//   const book = {
-//     id: books.length + 1,
-//     title: req.body.title,
-//     author: req.body.author,
-//     isbn: req.body.isbn,
-//     pages: req.body.pages,
-//     published: req.body.published,
-//   };
-//   books.push(book);
-//   res.send(book);
-// });
-
-// // app.put("/api/books/:id", (req, res) => {
-//   let id = +req.params.id;
-//   let body = req.body;
-//   let index = books.findIndex((b: { id: number }) => b.id === id);
-//   if (index >= 0) {
-//     let updatebook = { id: id, ...body };
-//     books[index] = updatebook;
-//     res.send(updatebook);
-//   } else {
-//     res
-//       .sendStatus(404)
-//       .send("book id could not be found..book is not updated!");
-//   }
-// });
-
-// app.delete("/api/books/:id", (req, res) => {
-//   const id: number = parseInt(req.params.id);
-//   const deleteBook = books.find((book: { id: number }) => book.id == id);
-//   if (deleteBook) {
-//     const book = books.indexOf(deleteBook);
-//     books.splice(book, 1);
-//     res.status(200).json("Book deleted!");
-//   } else {
-//     res.status(404).json("book id could not be found..book is not deleted!");
-//   }
-// });
 
 app.listen(3001, () => {
   console.log("server is running on: http://localhost:3001");

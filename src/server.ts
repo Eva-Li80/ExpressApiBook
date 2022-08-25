e59a6cf259761e6604954d4a74a028ff44b175c7
Eva-Li8E <evaliboden@gmail.com>
2022-08-25 14:23:08 +0200

    console log method and path

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import bookRouter from "./bookresourses/bookrouter";
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+app.use(express.static("./public"));
+
 app.use("/api/books", bookRouter);
 
 app.listen(3000, () => {
